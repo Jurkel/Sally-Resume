3bfa1feda171ccfe854a558192ceb4f24a8d3c8d
Harvey Le <harveyxcle@gmail.com>
2020-02-17 13:44:24 -0500

    first changes

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Split from './composition/Split.js';
+import './App.css';
 
 function App() {
-  return <main className='App'>{/* content goes here */}</main>;
+  return (
+    <main className='App'>
+      <Split className='left'>
+      This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+      </Split>
+      <Split className='right'>
+      This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
+    </main>
+  );
 }
 
 export default App;
