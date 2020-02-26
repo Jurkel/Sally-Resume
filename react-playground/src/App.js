ec2fdf967af50560cbfff8c9ccdf7bf71773774a
Harvey Le <harveyxcle@gmail.com>
2020-02-26 15:24:05 -0500

    created state folder

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,34 +1,19 @@
 import React from 'react';
-import Split from './composition/Split';
 import './App.css';
-import Tooltip from './composition/Tooltip';
+import TheDate from './state/TheDate';
 
-// make 2 tooltips here and another inside the App directly
-const firstTooltip = (
-  <Tooltip color='hotpink' message='tooltip message'>
-    ipsum
-  </Tooltip>
-)
-const secondTooltip = (
-  <Tooltip color='#126BCC' message='another tooltip message'>
-    officiis
-  </Tooltip>
-)
-
-function App() {
-  return (
-    <main className='App'>
-      <Split className='left' flexBasis='2'>
-        This is the content for the left `Split`. Lorem {firstTooltip} ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
-        <Tooltip message='one more tooltip message'>
-          Necessitatibus?
-        </Tooltip>
-      </Split>
-      <Split className='right'>
-        This is the content for the right `Split`. Inventore aliquid cupiditate {secondTooltip} suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-    </main>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>YOUR APPLICATION NAME!</h1>
+        <TheDate />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
