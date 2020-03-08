3a0b6dabd70f9eb8156a6f403ad63e22286ddf40
Harvey Le <harveyxcle@gmail.com>
2020-03-07 23:11:41 -0500

    finalized Bomb

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -6,7 +6,6 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <h1>YOUR APPLICATION NAME!</h1>
         <Bomb />
       </div>
     );
--- react-playground/src/state-drills/Bomb.js
+++ react-playground/src/state-drills/Bomb.js
@@ -2,24 +2,36 @@ import React from 'react';
 
 class Bomb extends React.Component {
   state = {
-    count: 0,
-    current: 'word'
+    count: 0
   }
   
   componentDidMount() {
     this.interval = setInterval(() => {
+      console.log('setInterval')
       this.setState({
         count: this.state.count + 1
       })
     }, 1000)
   }
+  renderDisplay() {
+    const { count } = this.state
+    if (count >= 8) {
+      clearInterval(this.interval)
+      return 'BOOM!!!!'
+    } else if (count % 2 === 0) {
+      return 'tick'
+    } else {
+      return 'tock'
+    }
+  }
   componentWillUnmount() {
     clearInterval(this.interval)
   }
+  
   render() {
     return (
       <div>
-        <p>{this.state.current}</p>
+        <p>{this.renderDisplay()}</p>
       </div>
     )
   }
