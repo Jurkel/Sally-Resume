c16fb136aab9794e4d606f189093cdc9a4ccf65d
Harvey Le <harveyxcle@gmail.com>
2020-03-09 10:26:32 -0400

    Added RouletteGun

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Bomb from './state-drills/Bomb'
+import RouletteGun from './state-drills/RouletteGun'
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Bomb />
+        <RouletteGun />
       </div>
     );
   }
