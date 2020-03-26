73d259d8dd43893796b7367789d2245ffd1220d9
Harvey Le <harveyxcle@gmail.com>
2020-03-26 18:10:55 -0400

    created denonym app

--- react-playground/src/Promises/Promises.js
+++ react-playground/src/Promises/Promises.js
@@ -1,3 +1,24 @@
-setTimeout(() => {
-  console.log('Hey! I waited 3 seconds.');
-}, 3000);
+function doTask(name) {
+  const p = new Promise((resolve, reject) => {
+    console.log(`${name} has started`);
+    const duration = Math.floor(Math.random() * 5000);
+    setTimeout(() => {
+      resolve(`${name} has ended after ${duration} milliseconds.`);
+    }, duration);
+  });
+  return p;
+}
+
+Promise
+  .all([
+    doTask('A'),
+    doTask('B'),
+    doTask('C')
+  ])
+  .then(results => {
+    results.forEach(result => console.log(result));
+    return doTask('D');
+  })
+  .then(result => {
+    console.log(result);
+  });
