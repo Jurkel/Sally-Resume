ac1febf27a583b544f1a5037a681beb692ef2e79
Harvey Le <harveyxcle@gmail.com>
2020-04-11 19:00:55 -0400

    updated buttons on lang switcher

--- react-playground/src/lang-context/LangControls.js
+++ react-playground/src/lang-context/LangControls.js
@@ -1,20 +1,34 @@
 import React from 'react'
+import LanguageContext from './LanguageContext'
 
 export default function LangControls(props) {
   return (
-    <>
-      <buton>
-        British{' '}
-        <span role='img' aria-label='en-GB'>🇬🇧</span>
-      </buton>
-      <buton>
-        American{' '}
-        <span role='img' aria-label='en-US'>🇺🇸</span>
-      </buton>
-      <buton>
-        Korean{' '}
-        <span role='img' aria-label='ko'>🇰🇷</span>
-      </buton>
-    </>
+    <LanguageContext.Consumer>
+      {(value) => {
+        return (
+          <>
+            <button
+              onClick={() => props.onSetLang('en-GB')}
+              disabled={value.lang === 'en-GB'}>
+              British{' '}
+              <span role='img' aria-label='en-GB'>🇬🇧</span>
+            </button>
+            <button
+              onClick={() => props.onSetLang('en-US')}
+              disabled={value.lang === 'en-US'}>
+              American{' '}
+              <span role='img' aria-label='en-US'>🇺🇸</span>
+            </button>
+            <button
+              onClick={() => props.onSetLang('ko')}
+              disabled={value.lang === 'ko'}>
+              Korean{' '}
+              <span role='img' aria-label='ko'>🇰🇷</span>
+            </button>
+          </>
+        )
+      }}
+    </LanguageContext.Consumer>
+    
   )
 }
