61b1d3bfe2aae297c45012a20c70e6fdb1f3fcdc
Harvey Le <harveyxcle@gmail.com>
2021-01-11 12:14:50 -0500

    updated hooks to count in color

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -1,22 +1,24 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
+import randomcolor from 'randomcolor'
 
 function App() {
     const [count, setCount] = useState(0)
-    const [answer, setAnswer] = useState("Yes")
+    const [color, setColor] = useState("")
     
-    function increment() {
-        setCount(prevCount => prevCount + 1)
-    }
-    
-    function decrement() {
-        setCount(prevCount => prevCount - 1)
-    }
-    
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCount(prevCount => prevCount + 1)
+        }, 1000)
+        return () => clearInterval(intervalId)
+    }, [])
+
+    useEffect(() => {
+        setColor(randomcolor())
+    }, [count])
+
     return (
         <div>
-            <h1>{count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
+            <h1 style={{color: color}}>{count}</h1>
         </div>
     )
 }
